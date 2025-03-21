ea22b32ab261975815774dbae1a2b792cec06eb9
KateAnufrieva <93079863+KateAnufrieva@users.noreply.github.com>
2025-03-21 21:17:53 +0300

    filters2

--- src/components/ui/SelectMovies/SelectMovies.jsx
+++ src/components/ui/SelectMovies/SelectMovies.jsx
@@ -1,3 +1,4 @@
+import { Title } from '@mui/icons-material';
 import CloseIcon from '@mui/icons-material/Close';
 import {
   Box,
@@ -10,7 +11,24 @@ import {
 } from '@mui/material';
 import React from 'react';
 
-export default function SelectMovies() {
+export default function SelectMovies({
+  countriesList,
+  genresList,
+  countries,
+  order,
+  year,
+  genreId,
+}) {
+  const ordersList = [
+    { title: 'По рейтингу', value: 'RATING' },
+    { title: 'По оценкам', value: 'NUM_VOTE' },
+  ];
+
+  const yearsList = new Array(60).fill(null).map((__, index) => ({
+    title: new Date().getFullYear() - index,
+    value: new Date().getFullYear() - index,
+  }));
+
   return (
     <Stack
       mt={2}
@@ -19,34 +37,42 @@ export default function SelectMovies() {
     >
       <FormControl fullWidth size="small">
         <InputLabel>Сортировка</InputLabel>
-        <Select label="Age">
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+        <Select label="Orders">
+          {ordersList.map(order => (
+            <MenuItem key={order.value} value={order.value}>
+              {order.title}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl fullWidth size="small">
         <InputLabel>Страна</InputLabel>
-        <Select label="Age">
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+        <Select label="Countries">
+          {countriesList.map(country => (
+            <MenuItem key={country.id} value={country.id}>
+              {country.country}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl fullWidth size="small">
         <InputLabel>Жанр</InputLabel>
-        <Select label="Age">
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+        <Select label="Genres">
+          {genresList.map(genre => (
+            <MenuItem key={genre.id} value={genre.id}>
+              {genre.genre}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl fullWidth size="small">
         <InputLabel>Год</InputLabel>
-        <Select label="Age">
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+        <Select label="Years">
+          {yearsList.map(year => (
+            <MenuItem key={year.value} value={year.value}>
+              {year.title}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
       <Box>
