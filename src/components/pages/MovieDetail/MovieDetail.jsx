7323cc694fb609b194bdf4ed247c8a8ae53d1446
KateAnufrieva <93079863+KateAnufrieva@users.noreply.github.com>
2025-03-24 19:12:47 +0300

    DetailPage1

--- src/components/pages/MovieDetail/MovieDetail.jsx
+++ src/components/pages/MovieDetail/MovieDetail.jsx
@@ -1,5 +1,40 @@
+import { Box, CircularProgress } from '@mui/material';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import {
+  useGetFilmQuery,
+  useGetSequelsAndPrequelsQuery,
+  useGetStaffQuery,
+} from '../../../services/kinopoiskApi';
+import ErrorMessage from '../../ui/ErrorMessage';
 
 export default function MovieDetail() {
+  const { id } = useParams();
+
+  const responseFilm = useGetFilmQuery(id);
+  const responseSequelsAndPrequel = useGetSequelsAndPrequelsQuery(id);
+  const responseStaff = useGetStaffQuery(id);
+
+  if (
+    responseFilm.isLoading ||
+    responseSequelsAndPrequel.isLoading ||
+    responseStaff.isLoading
+  ) {
+    return (
+      <Box display="flex" justifyContent="center" margin="auto">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (
+    responseFilm.error ||
+    responseSequelsAndPrequel.error ||
+    responseStaff.error
+  ) {
+    return <ErrorMessage />;
+  }
+
   return <div>MovieDetail</div>;
 }
